b0bbd0ca3bb9c46b8505440b7a17763a5fa4700b
Brady Taylor <baetay03@gmail.com>
2021-03-31 19:03:58 -0600

    js form task for code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskhandler = function () {
+var createTaskhandler = function (event) {
+console.log(event);
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "this is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskhandler);
+formEl.addEventListener("submit", createTaskhandler);
